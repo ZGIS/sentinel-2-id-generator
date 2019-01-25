8723397252190124ba514ad928cbd9363d46076d
whisperingpixel <martin.sudmanns@sbg.ac.at>
2019-01-25 16:38:15 +0100

    Added some checks for input. Fixes #5

--- s2_idgen/s2_idgen.py
+++ s2_idgen/s2_idgen.py
@@ -11,18 +11,99 @@ class Generator:
         :param acquisition_time: string (formatted as YYYYMMDDHHMMSS, e.g. '20170105T013442')
         """
         #
-        # User lowercase of
+        # User lowercase of granule ids
         #
-        self.granule_id = granule_id.lower()
-        self.acquisition_time = (parser.parse(acquisition_time)).timetuple()
+        self.granule_id = self.__checkGranuleId(granule_id)
+        
+        #
+        # convert time string to tuple
+        #
+        self.acquisition_time = self.__checkTimestamp(acquisition_time)
         
         #TODO: Check and sanitize inputs
+    
+
+    def __checkGranuleId(self, granule_id):
+        """
+        Checks whether the value for the granule id is reasonable or not
+
+        :param granule_id: string (formatted as Txxxxx, e.g 'T33UVP')
+        """
+
+        #
+        # Check for None and length
+        #
+        if granule_id is None or len(granule_id) != 6:
+             raise Exception('Invalid granule id {granule_id}'.format(granule_id = granule_id))
         
+        #
+        # Do the conversion
+        #        
+        granule_id_candidate = granule_id.lower()
+        
+        #
+        # Check several values
+        #                
+        if granule_id_candidate[0] != 't':
+            raise Exception('Invalid granule id {granule_id}. It need to start with "t", e.g. "T33UVP".'.
+                format(granule_id = granule_id))
+        
+        #
+        # Check several values
+        #               
+        if int(granule_id_candidate[1:2]) < 0 or int(granule_id_candidate[1:2]) > 66:
+            raise Exception('Invalid granule id {granule_id}. "T" needs to be followed by exactly two digits, e.g. "T33UVP".'.
+                format(granule_id = granule_id))
+        
+        for char in granule_id_candidate[3:6]:
+            if char.isalpha() == False:
+                raise Exception('Invalid granule id {granule_id}. The digits need to be followed by exactly three letters, e.g. "T33UVP".'.
+                    format(granule_id = granule_id))
+
+        #
+        # Return if it passed all tests
+        #
+        return granule_id_candidate
+    
+
+    def __checkTimestamp(self, acquisition_time):
+        """
+        Checks whether the value for the acquisition time is reasonable or not
+
+        :param acquisition_time: string (formatted as YYYYMMDDHHMMSS, e.g. '20170105T013442')
+        """
+
+        #
+        # Check for None
+        #
+        if acquisition_time is None:
+             raise Exception('Invalid acquisition_time {acquisition_time}'.
+                format(acquisition_time =acquisition_time))
+
+        #
+        # Do the conversion
+        #        
+        acquisition_time_candidate = (parser.parse(acquisition_time)).timetuple()
+
+        #
+        # Check several values
+        #          
+        if acquisition_time_candidate.tm_year < 2015:
+            raise Exception('Invalid year {year} in acquisition time {acquisition_time}'.
+                format(year = acquisition_time_candidate.tm_year, acquisition_time =acquisition_time))
+
+        #
+        # Return if it passed all tests
+        #
+        return acquisition_time_candidate
+
+
     def getID(self):
         """
         getId.
         return: string (formatted as {granule-id}-{acquisitionyear}-{acquisitiondayofyear}-{acquisitionhour}-{acquisitiontime as (minutes / 5)})
         """
+        
         acquisitionyear = self.acquisition_time.tm_year
         acquisitiondayofyear = self.acquisition_time.tm_yday
         acquisitionhour = self.acquisition_time.tm_hour
--- s2_idgen/test_s2_idgen.py
+++ s2_idgen/test_s2_idgen.py
@@ -9,6 +9,22 @@ class TestGenerator(unittest.TestCase):
     
     def test_getid(self):
         self.assertEqual(self.s2_id.getID(), 't33uvp-2017-005-01-6')
+
+
+class TestGenerator2(unittest.TestCase):
         
+    def test_checkgranuleid(self):
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = None, acquisition_time = '20170105T013442')
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = '33UVVP', acquisition_time = '20170105T013442')
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = 'T33UVVP', acquisition_time = '20170105T013442')
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = 'TXXXXX', acquisition_time = '20170105T013442')
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = 6, acquisition_time = '20170105T013442')
+
+    def test_checktimestamp(self):
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = 'T33UVP', acquisition_time = None)        
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = 'T33UVP', acquisition_time = '20140105T013442')
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = 'T33UVP', acquisition_time = 'somedummystuffhere')
+        self.assertRaises(Exception,s2_idgen.Generator,granule_id = 'T33UVP', acquisition_time = 1244)
+
 if __name__ == '__main__':
     unittest.main()
